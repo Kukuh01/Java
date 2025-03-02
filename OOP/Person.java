6e5582296b7d3e2fa57e5a07e24cf8701bb447b0
Kukuh01 <silvanusprasetyo@gmail.com>
2025-03-02 20:53:36 +0700

    Update materi OOP

--- OOP/Person.java
+++ OOP/Person.java
@@ -1,14 +1,21 @@
 public class Person {
 
 //Deklarasi variabel/property class
-String name;
-String address;
+String name = "Silvanus";
+String address = "Jl Ketileng";
 final String country = "Indonesia";
 
 //Method
 void sayHello(String name){
     System.out.println("Hello" + name);
 
+}
+
+//Constructor
+//Ketika membuat constructor wajib saat membuat object diberi parameternya
+Person(String paraName, String paraAddress){
+    name = paraName;
+    address = paraAddress;
 }
 
 
--- OOP/PersonApp.java
+++ OOP/PersonApp.java
@@ -3,12 +3,15 @@ public class PersonApp {
     public static void main(String[] args) {
 
         //Mengambil objek person di objek person
-        var person1 = new Person();
-        var person2 = new Person();
-        var person3 = new Person();
+        var person1 = new Person("Silvanus","ketileng");
+        var person2 = new Person("Ananta","Imam Bonjol");
+        var person3 = new Person("Roif", "Wonodri");
 
 
         person1.sayHello("Silvanus");
+        System.out.println(person1.name);
+        System.out.println(person1.address);
+        System.out.println(person1.country);
         
     }
 }
